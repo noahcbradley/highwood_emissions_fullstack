0da635b8c1fff3476cfca134fdb589f4496cd26b
Noah Bradley <n.bradley@shearfrac.com>
2026-02-10 15:30:30 -0700

    Working server with static data endpoints

--- src/server/src/sites/sites.controller.ts
+++ src/server/src/sites/sites.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SitesService } from './sites.service';
 
 @Controller('sites')
-export class SitesController {}
+export class SitesController {
+  constructor(private readonly sitesService: SitesService) {}
+
+  @Get()
+  getAll() {
+    return this.sitesService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.sitesService.findOne(Number(id));
+  }
+}
--- src/server/src/sites/sites.service.ts
+++ src/server/src/sites/sites.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SitesService {}
+export class SitesService {
+  private sites = [
+    { id: 1, name: 'Well Pad 1', emissionLimit: 100 },
+    { id: 2, name: 'Well Pad 2', emissionLimit: 200 },
+  ];
+
+  findAll() {
+    return this.sites;
+  }
+
+  findOne(id: number) {
+    return this.sites.find((site) => site.id === id);
+  }
+}
